c5865a8e0b9cd3bb458e89d2bdc33de117c65e9d
wuzheng <1345381396@qq.com>
2019-10-18 19:26:56 +0800

    my first commit,2019.10.18.19:26

---
+++ experiment-05/src/main/java/com/contoller/LoginServlet.java
@@ -0,0 +1,20 @@
+package com.contoller;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        req.getRequestDispatcher("/WEB-INF/jsp/login,html")
+                .forward(req, resp);
+    }
+}
